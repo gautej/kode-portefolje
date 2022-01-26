e82e802b78c1540c14a6222b5c24f4ca48448b13
gautej <68009833+gautej@users.noreply.github.com>
2022-01-26 19:51:52 +0100

    int

--- C++/main.cpp
+++ C++/main.cpp
@@ -1,5 +1,30 @@
 #include <iostream>
+#include <vector>
 
-int main(int, char**) {
-    std::cout << "Hello, world!\n";
+using namespace std;
+
+int main(int, char **)
+{
+    int n;
+    while (cin >> n)
+    {
+        cout << n << endl;
+
+        vector<int> v;
+        for (int i = 0; i < n; i++)
+        {
+            if (n % i == 0)
+                v.push_back(i);
+        }
+        int sum = 0;
+        for (int i : v)
+            sum += i;
+
+        if (sum == n)
+            cout << n << " perfect" << endl;
+        else if (sum - 2 <= n && sum + 2 >= n)
+            cout << n << " almost perfect" << endl;
+        else
+            cout << n << " not perfect" << endl;
+    }
 }
